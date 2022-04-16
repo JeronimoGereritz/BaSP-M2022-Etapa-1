363b7ffb800aa1c54bb4a24351b9d7624c2bbea0
jeronimo gereritz <jeronimogereritz@gmail.com>
2022-04-16 15:10:06 -0300

    BaSP-2022: First Exercise done, cant see the results in the page, gotta fix

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+var manzanas = 4;
+var naranjas = 7;
+
+var manzanasMasNaranjas = manzanas + naranjas;
+
+console.log(manzanasMasNaranjas);
+
+/*** */
+
+var Omar = "Omar ama a"
+var Monica = "Monica"
+
+console.log(Omar, Monica)
+
+
+/*** */
+
+var Ammo = "Ammunition"
+var Ar = "Assault Rifle"
+
+console.log(Ammo.length, Ar.length)
